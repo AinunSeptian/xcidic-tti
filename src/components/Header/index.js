49e2164e7fea868a16ff37da2ab83c31173abf00
dbsSensei <dimasbagussusilo@gmail.com>
2021-06-10 21:08:41 +0700

    Refactoring and fixing bad-code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,61 +1,71 @@
-import React from "react";
-import "./header.css";
-import NavVector1 from "../../assets/images/nav-vector-1.png";
-import NavVector2 from "../../assets/images/nav-vector.png";
-import logoNavbar from "../../assets/images/vector-navbar-tti.png";
+import React from 'react';
+import classes from './header.module.css';
+import NavVector1 from '../../assets/img/nav-vector-1.png';
+import NavVector2 from '../../assets/img/nav-vector.png';
+import logoNavbar from '../../assets/img/vector-navbar-tti.png';
 
 const Header = () => {
   return (
-    <nav className="wrapper-header">
-      <div className="wrapper-logo">
-        <img src={logoNavbar} alt="" className="img-logo" />
-      </div>
-      <div className="wrapper-nav-text">
-        <a href="" className="nav-text">
-          Home
-        </a>
-        <a href="" className="nav-text">
-          About
-        </a>
-        <a href="" className="nav-text">
-          Trade
-        </a>
-        <a href="" className="nav-text">
-          Investment
-        </a>
-        <a href="" className="nav-text">
-          Tourism
-        </a>
-        <a href="" className="nav-text-vector">
-          Regional Cluster
-          <img
-            src={NavVector1}
-            alt="arah panah bawah"
-            className="img-nav-vector-1"
-          />
-        </a>
-        <a href="" className="nav-text-vector">
-          News & Publications
-          <img
-            src={NavVector1}
-            alt="arah panah bawah"
-            className="img-nav-vector-1"
-          />
-        </a>
-        <a href="" className="nav-text">
-          FAQs
-        </a>
-        <a href="" className="nav-text-vector">
-          <img
-            src={NavVector2}
-            alt="arah panah bawah"
-            className="img-nav-vector-2"
-          />
-          US/EN
-        </a>
-        <button className="btn-nav">Login/Signup</button>
-      </div>
-    </nav>
+    <header className={classes.header}>
+      <a className={classes.logo}>
+        <img src={logoNavbar} alt='TTI Logo' className={classes.image} />
+      </a>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#'>About</a>
+          </li>
+          <li>
+            <a href='#'>Trade</a>
+          </li>
+          <li>
+            <a href='#'>Investment</a>
+          </li>
+          <li>
+            <a href='#'>Tourism</a>
+          </li>
+          <li>
+            <a href='#' className={classes['nav-text-vector']}>
+              Regional Cluster
+              <img
+                src={NavVector1}
+                alt='down arrow'
+                className={classes['img-nav-vector-1']}
+              />
+            </a>
+          </li>
+          <li>
+            <a href='#' className={classes['nav-text-vector']}>
+              News & Publications
+              <img
+                src={NavVector1}
+                alt='down arrow'
+                className={classes['img-nav-vector-1']}
+              />
+            </a>
+          </li>
+          <li>
+            <a href='#'>FAQs</a>
+          </li>
+          <li>
+            <a href='#' className={classes['nav-text-vector']}>
+              <img
+                src={NavVector2}
+                alt='down arrow'
+                className={classes['img-nav-vector-2']}
+              />
+              US/EN
+            </a>
+          </li>
+          <li>
+            <button className={classes.button}>Login/Signup</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
