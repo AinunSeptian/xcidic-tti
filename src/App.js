77ced649c3cddfb82959dd9b26bf15fe6de8e902
Ainun Septian <ainunseptian79@gmail.com>
2021-06-10 11:33:53 +0700

    create nav header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./component/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
