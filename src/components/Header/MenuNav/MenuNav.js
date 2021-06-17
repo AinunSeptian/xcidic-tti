45147b1fefd7833082d205a45ff3e5695cd3b9e5
rhobbikhaidir <rhobbie09@gmail.com>
2021-06-17 15:55:48 +0700

    addding backdropp in project

--- src/components/Header/MenuNav/MenuNav.js
+++ src/components/Header/MenuNav/MenuNav.js
@@ -1,59 +1,60 @@
-import React, { useState, useRef, Fragment } from 'react';
-import classes from './MenuNav.module.css';
-import MenuNavIcon from '../../../assets/img/menu-nav-icon.svg';
-import languageNavMenu from '../../../assets/img/lang-nav-menu-icon.png';
+import React, { useState, useRef } from "react";
+import classes from "./MenuNav.module.css";
+import MenuNavIcon from "../../../assets/img/menu-nav-icon.svg";
+import languageNavMenu from "../../../assets/img/lang-nav-menu-icon.png";
 
-import useOnClickOutside from '../../../hooks/useOnClickOutside';
+import useOnClickOutside from "../../../hooks/useOnClickOutside";
 
-const MenuNav = () => {
-  const modalRef = useRef();
-  const [isModalOpen, setModalOpen] = useState(false);
-
-  useOnClickOutside(modalRef, () => setModalOpen(false));
-
+const MenuNav = ({ isModalOpen, setModalOpen, modalRef }) => {
   return (
-    <div
-      onClick={() => setModalOpen(true)}
-      className={classes.containerNavMenu}
-    >
-      {!isModalOpen && (
-        <img
-          src={MenuNavIcon}
-          alt=''
-          className={classes.menuNav1}
-          onClick={setModalOpen}
-        />
-      )}
+    <>
+      <div
+        onClick={() => setModalOpen(true)}
+        className={classes.containerNavMenu}
+      >
+        {isModalOpen === false ? (
+          <img
+            src={MenuNavIcon}
+            alt=""
+            className={classes.menuNav1}
+            onClick={setModalOpen}
+          />
+        ) : (
+          ""
+        )}
 
-      {isModalOpen && (
-        <Fragment>
-          <div ref={modalRef} className={classes.front}>
-            <div className={classes.wrapperNavMenu}>
-              <p>
-                <img src={languageNavMenu} alt='world' />
-                US/EN
-              </p>
-              <ul>
-                <li>Login/SignUp</li>
-                <li>Home</li>
-                <li>About</li>
-                <li>Trade</li>
-                <li>Investment</li>
-                <li>Tourism</li>
-                <li>Regional Cluster</li>
-                <li>News & Publications</li>
-                <li>FAQs</li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
+        {isModalOpen && (
+          <>
+            <div ref={modalRef} className={classes.front}>
+              <div className={classes.wrapperNavMenu}>
+                <div className={classes.wrapperTextLanguage}>
+                  <p>
+                    <img src={languageNavMenu} alt="world" />
+                    US/EN
+                  </p>
+                </div>
+                <ul>
+                  <li>Login/SignUp</li>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Trade</li>
+                  <li>Investment</li>
+                  <li>Tourism</li>
+                  <li>Regional Cluster</li>
+                  <li>News & Publications</li>
+                  <li>FAQs</li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
             </div>
-          </div>
-          <div className={classes.backdrop}></div>
-        </Fragment>
-      )}
-    </div>
+            <div className={classes.backdrop}></div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/components/UI/NavBar/DropDown.js
+++ src/components/UI/NavBar/DropDown.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import "./DropDown.css";
-import asd from "../../../assets/img/nav-vector-1.png";
+import arrowDown from "../../../assets/img/nav-vector-1.png";
 export default function ModalNav(props) {
   return (
     <div className="dropdown">
       <button className="dropbtn">
         {props.title}
-        <img src={asd} alt="down arrow" className="img-vector" />
+        <img src={arrowDown} alt="down arrow" className="img-vector" />
       </button>
       <div className="dropdown-content">
         <a href="#">Link 1</a>
